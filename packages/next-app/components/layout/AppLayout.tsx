0fb1f04273d154b634006ecca6977cc84ff04bef
e-roy <ericroupe@gmail.com>
2022-04-11 20:37:44 -0400

    fixed coming soon

--- packages/next-app/components/layout/AppLayout.tsx
+++ packages/next-app/components/layout/AppLayout.tsx
@@ -1,14 +1,19 @@
 import { Header } from "@/components/layout";
+import { BetaView } from "@/components/layout";
 
 type AppLayoutProps = {
   children: React.ReactNode;
 };
 
 export const AppLayout = ({ children }: AppLayoutProps) => {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-1">{children}</main>
-    </div>
-  );
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+    return (
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-1">{children}</main>
+      </div>
+    );
+  } else {
+    return <BetaView />;
+  }
 };
